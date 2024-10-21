91ff02978e177c4da119147e5773018d1ed5a241
jiangyan03 <1349756130@qq.com>
2024-10-21 19:42:18 +0800

    0.2.3

--- plugin/bedrock_liteloader_handler.py
+++ plugin/bedrock_liteloader_handler.py
@@ -5,10 +5,12 @@ from typing_extensions import override
 
 from mcdreforged.handler.impl import AbstractMinecraftHandler
 from mcdreforged.info_reactor.server_information import ServerInformation
-from mcdreforged.minecraft.rtext.text import RTextBase
+from mcdreforged.minecraft.rtext.text import RTextBase, RText, RTextList, RTextTranslation
 from mcdreforged.plugin.meta.version import VersionParsingError
+from mcdreforged.plugin.si.server_interface import ServerInterface
 from mcdreforged.utils.types.message import MessageText
 from mcdreforged.info_reactor.info import Info
+from mcdreforged.plugin.meta.version import Version
 
 """
 bedrock server handler
@@ -16,7 +18,7 @@ bedrock server handler
 
 PLUGIN_METADATA = {
     'id': 'bedrock_server',
-    'version': '0.2.0',
+    'version': '0.2.3',
     'name': 'handling BDS with liteloader modded',
     'description': 'A plugin for bedrock server',
     'author': 'jiangyan03, Elec_glacier',
@@ -44,18 +46,9 @@ class BedrockServerHandler(AbstractMinecraftHandler):
     def get_name(self) -> str:
         return 'liteloader_handler'
 
-    # MCDR 获取服务端信息
-    @override
-    def pre_parse_server_stdout(self, text: str) -> str:
-        cleaned_text = ''.join(char for char in text if char not in '\x08\x00-\x1F\x7F')
-        print(cleaned_text)
-        return cleaned_text
-
     # [18:23:50 Info] [Server] IPv4 supported, port: 19132    ->ll1
     # 18:25:14 INFO [Server] IPv4 supported, port: 19132: Used for gameplay and LAN discovery   ->ll2
     # 18:25:58.195 INFO [Server] Server started in (3.2s)! For help, type "help" or "?"   ->ll3
-
-
 
     @classmethod
     @override
@@ -64,7 +57,7 @@ class BedrockServerHandler(AbstractMinecraftHandler):
         不同版本不同日志输出
         eg:
         return re.compile(
-            r'\[?(?P<hour>\d{2}):(?P<min>\d{2}):(?P<sec>\d{2})\s'
+            r'>?\s?\[?(?P<hour>\d{2}):(?P<min>\d{2}):(?P<sec>\d{2})\s'
             r'(?P<logging>\w+)'
             r']?\s?'
             r'(\[[^]]+])\s'  # thread -> P<thread> not use
@@ -73,23 +66,11 @@ class BedrockServerHandler(AbstractMinecraftHandler):
         """
         pass
 
-
     # @classmethod
     # @override
     # def get_content_parsing_formatter(cls) -> re.Pattern:
     #     return re.compile(
-    #         r'(?P<hour>\d{2}):(?P<min>\d{2}):(?P<sec>\d{2}) '
-    #         r'(?P<logging>\w+)'
-    #         r'( \[[^]]+])'  # thread -> P<thread> #r' (?:\[[^\]]+\])? '
-    #         r' (?P<content>.*)'
-    #     )
-
-
-    # @classmethod
-    # @override
-    # def get_content_parsing_formatter(cls) -> re.Pattern:
-    #     return re.compile(
-    #         r'\[?(?P<hour>\d{2}):(?P<min>\d{2}):(?P<sec>\d{2})\s'
+    #         r'>?\s?\[?(?P<hour>\d{2}):(?P<min>\d{2}):(?P<sec>\d{2})\s'
     #         r'(?P<logging>\w+)'
     #         r']?\s?'
     #         r'(\[[^]]+])\s'  # thread -> P<thread> not use
@@ -103,36 +84,39 @@ class BedrockServerHandler(AbstractMinecraftHandler):
 
     @override
     def get_send_message_command(self, target: str, message: MessageText, server_information: ServerInformation):
-        return 'tellraw “{}” {}'.format(target, self.format_message(message))
-        # can_do_execute = False
-        # if server_information.version is not None:
-        #     try:
-        #         from mcdreforged.plugin.meta.version import Version
-        #         version = Version(server_information.version.split(' ')[0])
-        #         if version >= Version('1.19.50'):
-        #             can_do_execute = True
-        #     except VersionParsingError:
-        #         pass
-        # command = 'tellraw {} {}'.format(target, self.format_message(message))
-        # if can_do_execute:
-        #     command = 'execute at @p run ' + command
-        # return command
+        can_do_execute = False
+        if server_information.version is not None:
+            try:
+                # from mcdreforged.plugin.meta.version import Version
+                version = Version(server_information.version.split(' ')[0])
+                if version >= Version('1.19.50.0'):
+                    can_do_execute = True
+            except VersionParsingError:
+                pass
+        command = 'tellraw {} {}'.format(target, self.format_message(message))
+        if can_do_execute:
+            command = 'execute at @p run ' + command
+        return command
 
     # -------------------------
     #   Server output parsing
     # -------------------------
 
-    # 信息添加处理，暂时不处理
-    # @override
-    # def get_broadcast_message_command(self, message: MessageText, server_information: ServerInformation):
-    #     return self.get_send_message_command('@a', message, server_information)
+    # MCDR 获取服务端信息
+    @override
+    def pre_parse_server_stdout(self, text: str) -> str:
+        # 除去特殊字符串，由于获取1.18.2输出的时候会出现\x08这种操作符，故需要预处理一下服务端的输出
+        cleaned_text = ''.join(char for char in text if char not in '\x08\x00-\x1F\x7F')
+        return cleaned_text
 
     # 服务端信息分离，没有特殊修改勿动
     @override
     def parse_server_stdout(self, text: str):
         result = super().parse_server_stdout(text)
+        # 由于xboxid存在\s空格字符串，玩家id必须引号处理命令
+        if result.player is not None:
+            result.player = '"' + result.player + '"'
         return result
-        pass
 
     __player_joined_regex = re.compile(r'Player Spawned: (?P<name>[^>]+) xuid: (?P<xuid>\d+)')
 
@@ -141,7 +125,7 @@ class BedrockServerHandler(AbstractMinecraftHandler):
         if not info.is_user:
             if (m := self.__player_joined_regex.fullmatch(info.content)) is not None:
                 if self._verify_player_name(m['name']):
-                    return m['name']
+                    return '"' + m['name'] + '"'
         return None
 
     __player_left_regex = re.compile(r'Player disconnected: (?P<name>[^>]+), xuid: (?P<xuid>\d+)')
@@ -151,7 +135,7 @@ class BedrockServerHandler(AbstractMinecraftHandler):
         if not info.is_user:
             if (m := self.__player_left_regex.fullmatch(info.content)) is not None:
                 if self._verify_player_name(m['name']):
-                    return m['name']
+                    return '"' + m['name'] + '"'
         return None
 
     # Version 1.18.2.03(ProtocolVersion 475) with LiteLoaderBDS 2.0.9
@@ -176,8 +160,11 @@ class BedrockServerHandler(AbstractMinecraftHandler):
     @override
     def parse_server_address(self, info: Info):
         if not info.is_user:
-            if (m := self.__server_address_regex.fullmatch(info.content)) is not None:
-                return "127.0.0.1", int(m['port'])
+            server_information: ServerInformation = ServerInterface.get_instance().get_server_information()
+            if server_information.port is None: # 1.19以下的bds会输出两个ipv4
+                if (m := self.__server_address_regex.fullmatch(info.content)) is not None:
+                    # 基岩板无法获取ip哦
+                    return "127.0.0.1", int(m['port'])
         return None
 
     # ***检测日志知道服务器的启动完成
@@ -206,20 +193,77 @@ class BedrockServerHandler(AbstractMinecraftHandler):
 
     @override
     def format_message(self, message: MessageText) -> str:
-        """
-        A utility method to convert a message into a valid argument used in message sending command
-        """
-        # print(message)
-        # replace_dict = {
-        #     '↻': '[R]',  # 替换为 [R] 表示刷新
-        #     '↓': '[V]',  # 替换为 [V] 表示下载
-        #     '×': '[X]',  # 替换为 [X] 表示关闭
-        # }
+        # 获取服务端的编码暂时搁置，后续写
+        # 由于ll2一部分服务端会出现gbk输入，所以为了防止utf8字符变成gbk出现编码错误，需要预处理输入命令
+        def replace_utf8_chars(message_):
+            replace_dict = {
+                '↻': 'R',  # 替换为 [R] 表示刷新
+                '↓': 'D',  # 替换为 [V] 表示下载
+                '×': 'X',  # 替换为 [X] 表示关闭
+            }
+            # 替换列表中每个字符串元素
+            if isinstance(message_, RTextBase):
+                message_str = str(message_)
+                for old, new in replace_dict.items():
+                    updated_text = message_str.replace(old, new)
+                    message_str = updated_text
+                return message_str
+            return message_
+
+        try:
+            message = replace_utf8_chars(message)
+        except:
+            pass
+
+        # RText需要修改的内容太多了，beje的命令格式不一致，需要大改，只能暂时直接输入命令了
+        # if isinstance(message, RTextBase):
+        #     print('RTextBase')
+        #     print(message.to_json_str())
+        # else:
+        #     print('ELSE')
+        #     print(json.dumps(str(message)))
         message = f"{{\"rawtext\":[{{\"text\":\"{message}\"}}]}}"
         # 我知道这样很蠢，但是这只是权宜之计（，后续修改
         # print(message)
         return message
 
+    # 后续独立优化重写
+    # def replace_special_chars(self, message):
+    #     """
+    #     替换字符串或 RTextBase 对象中的特殊字符。
+    #     递归处理 RTextBase 类型对象中的特殊字符，并将其替换为指定字符。
+    #     :param message: 字符串或 RTextBase 类型的对象
+    #     :return: 替换后的字符串或 RTextBase 对象
+    #     """
+    #     replace_dict = {
+    #         '↻': 'R',  # 替换为 R 表示刷新
+    #         '↓': 'D',  # 替换为 D 表示下载
+    #         '×': 'X',  # 替换为 X 表示关闭
+    #     }
+    #
+    #     def replace_special_chars_in_text(text: str) -> str:
+    #         for old, new in replace_dict.items():
+    #             text = text.replace(old, new)
+    #         return text
+    #
+    #     if isinstance(message, str):
+    #         # 普通字符串
+    #         return replace_special_chars_in_text(message)
+    #     elif isinstance(message, RText):
+    #         # RText
+    #         new_text = replace_special_chars_in_text(str(message))
+    #         return RText(new_text, message.color, message.style)
+    #     elif isinstance(message, RTextList):
+    #         # RTextList ，递归替换
+    #         new_rtext_list = RTextList(*[self.replace_special_chars(item) for item in message])
+    #         return new_rtext_list
+    #     elif isinstance(message, RTextTranslation):
+    #         # RTextTranslation ，递归替换
+    #         new_args = [self.replace_special_chars(arg) if isinstance(arg, RTextBase) else arg for arg in message.args]
+    #         return RTextTranslation(message.translation_key, *new_args)
+    #     else:
+    #         # other
+    #         return message
 
 def on_load(server, prev_module):
     server.register_server_handler(BedrockServerHandler())
